76f2a3913120947dc88b0dbcfcb8d7809e172fbc
KirisameKei <53395204+KirisameKei@users.noreply.github.com>
2020-06-22 12:56:56 +0900

    これ要らんやろ

--- custom_commands.py
+++
@@ -1,5 +0,0 @@
-import discord
-
-async def on_message(client1, message, custom_commands_dict):
-    guild = client1.get_guild(message.guild.id)
-    channel = client1.get_channel(message.channel.id)

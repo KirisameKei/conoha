6d0d0e933a531366827d6f1c83d2da568d95230f
KirisameKei <53395204+KirisameKei@users.noreply.github.com>
2022-02-10 18:53:20 +0900

    いろは鯖(MC)の事前BAN者キック装置を削除、なんで残ってたの・・・

--- iroha_MC.py
+++ iroha_MC.py
@@ -5,11 +5,13 @@ import bs4
 import discord
 import requests
 
+
 async def on_member_join(client1, member):
     """
     いろは鯖に新規が来た時用役職
     user_data_dict[f"{member.id}"][ban]がtrueならキックする"""
 
+    """
     with open("./datas/user_data.json", mode="r") as f:
         user_data_dict = json.load(f)
 
@@ -19,7 +21,7 @@ async def on_member_join(client1, member):
             await member.guild.kick(member)
             await join_leave_notice_ch.send(f"{member.mention}が{member.guild.name}に参加しようとしましたが失敗しました")
     except KeyError:
-        pass
+        pass"""
 
 
 async def on_message(client1, message):
